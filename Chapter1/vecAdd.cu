e9f937e0eecb43f272d489d07cbfff2f04121cd6
mchoccie <minijm06@gmail.com>
2025-03-29 15:48:04 -0700

    Completed chapter 1

--- Chapter1/vecAdd.cu
+++ Chapter1/vecAdd.cu
@@ -1,3 +1,17 @@
+__global___ void vecAddKernel(float * A, float * B, float * c, int n){
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < n) {
+        c[i] = A[i] + B[i];
+    }
+}
+#include <stdio.h>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <chrono>
+
 void vecAdd(float * A_h, float * B_h, float * c_h, int n){
     int size = n * sizeof(float);
     float * A_d, * B_d, * c_d;
@@ -6,4 +20,10 @@ void vecAdd(float * A_h, float * B_h, float * c_h, int n){
     cudaMalloc((void**)&c_d, size);
     cudaMemcpy(A_d, A_h, size, cudaMemcpyHostToDevice);
     cudaMemcpy(B_d, B_h, size, cudaMemcpyHostToDevice);
+
+    vecAddKernel<<<ceil(n/256.0), 256>>>(A_d, B_d, c_d, n);
+    cudaMemcpy(c_h, c_d, size, cudaMemcpyDeviceToHost);
+    cudaFree(A_d);
+    cudaFree(B_d);
+    cudaFree(c_d);
 }
